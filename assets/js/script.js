495c5272f6777092bf52f3958f7cddcddf4a4a2e
isukhetskyi <ivan.sukhetskyi@gmail.com>
2017-05-14 20:40:57 +0300

    Hard and Soft skills list

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,12 @@ $( document ).ready(function() {
                     $("#index").fadeIn();
                     $('#index_left').addClass('animated slideInLeft');
                     $('#index_right').addClass('animated slideInRight');
-                    });
-           
+                });
+                
+                $('[id^=detail-]').hide();
+                $('.toggle').click(function() {
+                    $input = $( this );
+                    $target = $('#'+$input.attr('data-toggle'));
+                    $target.slideToggle();
+                });
 		});
